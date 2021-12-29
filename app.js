840254b5f0cc608ee902de527158e5087701db25
Cristian Bello <cristian.bello6@gmail.com>
2021-12-28 21:43:10 -0500

    add for loop

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  let parr = "";
+  let html = "";
+  for (let i = 0; i <= 50; i++) {
+    if (i % 2 === 0) {
+      parr = `<p>${i} Soy Par!</p>`;
+      html += parr;
+    } else {
+      parr = `<p>${i} Soy Impar!</p>`;
+      html += parr;
+    }
+  }
+  res.send(html);
+});
+
+app.listen(3000, () => console.log("Listening on port 3000!"));
